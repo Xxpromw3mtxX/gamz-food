d721bf2de01304fe23beb67a779099e124f49fd9
Francesco Metta <fra.metta@hotmail.it>
2021-09-05 14:26:39 +0200

    Added money checks

--- gamz-food/server/main.lua
+++ gamz-food/server/main.lua
@@ -2,12 +2,16 @@ local ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-
-
 RegisterServerEvent("gamz-food:removeMoney")
-AddEventHandler("gamz-food:removeMoney", function(money)
+AddEventHandler("gamz-food:removeMoney", function(money, prop, type)
     local src = source
     local xPlayer = ESX.GetPlayerFromId(src)
-
-    xPlayer.removeMoney(money)
+    local pMoney = xPlayer.getAccount('money').money 
+    
+    if pMoney >= money then
+        xPlayer.removeAccountMoney('money', money)
+        TriggerClientEvent('gamz-food:consumeFood', src, prop, type)
+    else
+        xPlayer.showNotification("Non hai abbastanza soldi!")
+    end
 end)
